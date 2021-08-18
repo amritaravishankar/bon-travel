1041493e857144da80405c540f523b4e6dd98673
amritaravishankar <amritaravishankar00@gmail.com>
2021-08-18 14:27:22 +0800

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+import List from './components/List/List';
+import {getPlacesData} from './api';
+
+const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null)
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState([]);
+
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [autocomplete, setAutocomplete] = useState(null);
+
+    useEffect (() => {
+        navigator.geolocation.getCurrentPosition(( {coords : {latitude, longitude}}) => {
+            setCoordinates({lat: latitude, lng: longitude});
+        })
+    })
+
+    useEffect(() => {
+        const filtered = places?.filter((place) => Number(place.rating) > rating);
+    
+        setFilteredPlaces(filtered);
+      }, [rating]);
+
+    useEffect (() => {
+        if (bounds.sw && bounds.ne) {
+            setIsLoading(true);
+
+            getPlacesData(type, bounds.sw, bounds.ne).then((data)=> {
+                setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                setFilteredPlaces([]);
+                setRating('');
+                setIsLoading(false);
+            });
+        }
+    }, [type, bounds]);
+    const onLoad = (autoC) => setAutocomplete(autoC);
+
+    const onPlaceChanged = () => {
+        const lat = autocomplete.getPlace().geometry.location.lat();
+        const lng = autocomplete.getPlace().geometry.location.lng();
+
+        setCoordinates({ lat, lng });
+  };
+
+    return (
+        <>
+            <CssBaseline />
+            <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List 
+                    places={filteredPlaces.length ? filteredPlaces : places}
+                    childClicked = {childClicked} 
+                    isLoading = {isLoading}
+                    type = {type}
+                    setType = {setType}
+                    rating = {rating}
+                    setRating = {setRating}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                    setCoordinates={setCoordinates} 
+                    setBounds={setBounds} 
+                    coordinates={coordinates}
+                    places={filteredPlaces.length ? filteredPlaces : places}
+                    setChildClicked={setChildClicked}/>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default App;
