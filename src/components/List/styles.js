6f944a7b71e4bf4d1213e040f4d8b70f5d2ba3fc
amritaravishankar <amritaravishankar00@gmail.com>
2021-08-19 22:48:16 +0800

    Change css

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -2,16 +2,17 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   formControl: {
-    margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
+    margin: theme.spacing(1), minWidth: 120, marginBottom: '30px', backgroundColor: '#0B285E', 
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
   loading: {
-    height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+    height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: '#0B285E', backgroundColor: '#0B285E',
   },
   container: {
     padding: '25px',
+    backgroundColor: '#0B285E',
   },
   marginBottom: {
     marginBottom: '30px',
@@ -19,4 +20,12 @@ export default makeStyles((theme) => ({
   list: {
     height: '75vh', overflow: 'auto',
   },
+  text: {
+    color: 'white',
+    paddingBottom: '30px'
+  },
+  optionText : {
+    color: 'white',
+    backgroundColor: '#0B285E',
+  }
 }));
